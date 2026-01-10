ee92907afd67f06b28c38aa832e7dfec0825e2f2
gishd2584 <951622481@qq.com>
2026-01-10 17:34:48 +0800

    加入了ddrnet、pidnet的训练配置

---
+++ configs/Acenzx/ddrnet_23_in1k-pre_4xb8-100epoch_opsfloodnet-512x512.py
@@ -0,0 +1,69 @@
+_base_ = [
+    '../_base_/datasets/opsfloodnet.py',
+    '../_base_/default_runtime.py','../_base_/schedules/epoch_100.py'
+]
+
+crop_size = (512, 512)
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    size=crop_size,
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255)
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    backbone=dict(
+        type='DDRNet',
+        in_channels=3,
+        channels=64,
+        ppm_channels=128,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        ),
+    decode_head=dict(
+        type='DDRHead',
+        in_channels=64 * 4,
+        channels=128,
+        dropout_ratio=0.,
+        num_classes=10,
+        align_corners=False,
+        norm_cfg=norm_cfg,
+        loss_decode=[
+            dict(
+                type='OhemCrossEntropy',
+                thres=0.9,
+                min_kept=131072,
+                
+                loss_weight=1.0),
+            dict(
+                type='OhemCrossEntropy',
+                thres=0.9,
+                min_kept=131072,
+                loss_weight=0.4),
+        ]),
+
+    # model training and testing settings
+    test_cfg=dict(mode='whole'))
+
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+
+train_dataloader = dict(batch_size=8, num_workers=4)
+val_dataloader = dict(batch_size=1, num_workers=4)
+
+
+test_dataloader = None
+test_cfg = None
+test_evaluator = None
+
+
+
+
+
+
