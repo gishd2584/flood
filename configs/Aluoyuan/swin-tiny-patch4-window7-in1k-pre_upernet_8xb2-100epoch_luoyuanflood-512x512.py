8ef876368069c504d10379d03f449b1373dff656
gishd2584 <951622481@qq.com>
2026-05-06 11:35:52 +0800

    添加swinTransform训练配置

---
+++ configs/Aluoyuan/swin-tiny-patch4-window7-in1k-pre_upernet_8xb2-100epoch_luoyuanflood-512x512.py
@@ -0,0 +1,53 @@
+_base_ = [
+    '../_base_/models/upernet_swin.py', '../_base_/datasets/luoyuanflood.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/epoch_100.py'
+]
+
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_tiny_patch4_window7_224_20220317-1cdeb081.pth'  # noqa
+
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file),
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        use_abs_pos_embed=False,
+        drop_path_rate=0.3,
+        patch_norm=True),
+    decode_head=dict(in_channels=[96, 192, 384, 768], num_classes=3),
+    auxiliary_head=dict(in_channels=384, num_classes=3))
+
+# AdamW optimizer, no weight decay for position embedding & layer norm
+# in backbone
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+
+param_scheduler = [
+    dict(type='LinearLR', by_epoch=True, start_factor=0.1, begin=0, end=5),
+    dict(
+        type='PolyLR',
+        eta_min=1e-4,
+        power=0.9,
+        begin=5,
+        end=100,
+        by_epoch=True,
+    )
+]
+
+# By default, swin models are trained with smaller batch size due to memory constraints
+train_dataloader = dict(batch_size=2)
+val_dataloader = dict(batch_size=1)
+test_dataloader = val_dataloader
